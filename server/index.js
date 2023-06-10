4775cf933f6610eb6cf46d916f8845bc757dec72
seonjakim <claire.kim@sendbird.com>
2023-06-10 18:02:19 +0900

    chore: add package.json in server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const { createTrialAccount } = require("./create-trial-drop");
 var app = express();
 const cors = require("cors");
 
-async function getTrialAccount() {}
 app.use(cors());
 app.get("/", async (req, res) => {
   const result = await createTrialAccount();
